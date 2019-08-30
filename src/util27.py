9fadcfcc3c75ccbf20b8a8c1b4b999b057167433
HeatherAda <1911196013@qq.com>
2019-08-29 21:02:37 -0400

    Add files via upload

--- src/util27.py
+++ src/util27.py
@@ -8,9 +8,9 @@ import xml.etree.ElementTree as ET
 load the data from dec theory
 '''
 
-def loadDecData(dataset,filepath):
 
-    #util.main(dataset)
+def loadDecData(dataset, filepath):
+    # util.main(dataset)
     mat, rel, orin2idx_instance = loadDec_Feat_Turk(dataset, filepath)  # mat: # tf-idf feature; rel: label 0/1
 
     responses, orin2idx_oracle = loadResponse(dataset, filepath)
@@ -18,8 +18,8 @@ def loadDecData(dataset,filepath):
     # responses (outside) -> (inside)
     responses = responseOut2In(responses, orin2idx_oracle, orin2idx_instance)
 
-    #filter_labels = {0, 1}
-    #matrix_res, oracle_idx, instance_idx, instance_feature =
+    # filter_labels = {0, 1}
+    # matrix_res, oracle_idx, instance_idx, instance_feature =
     u_features = np.array([[1] for i in range(len(orin2idx_oracle))])
     u_features, v_features, responses = u_features, mat, responses
 
@@ -30,40 +30,38 @@ def loadDecData(dataset,filepath):
     return u_features, v_features, responses, matrix_observed, label_idx, np.array(rel)
 
 
-def loadDec_Feat_Turk(dataset,filepath):
+def loadDec_Feat_Turk(dataset, filepath):
     if dataset == 'proton-beam':
         pub_dic = get_pub_dic_xml(filepath)
         # pub_dic_items are already sorted by key
         [rec_nums, texts] = zip(*pub_dic.items())
         rel = get_relevant(filepath)
     else:
-        pub_dic = get_pub_dic_csv(dataset,filepath)  # [Instance feature] {abstract_id:  title + abstract}
+        pub_dic = get_pub_dic_csv(dataset, filepath)  # [Instance feature] {abstract_id:  title + abstract}
         # [rec_nums, texts] = zip(*pub_dic.items())
-        (turk_dic, rel_dic) = get_turk_data(dataset)  # [Response] turk_dic: {abstract ID: [(Q3,Q4)]}; rel_dic: {abstract, label}
+        (turk_dic, rel_dic) = get_turk_data(
+            dataset)  # [Response] turk_dic: {abstract ID: [(Q3,Q4)]}; rel_dic: {abstract, label}
         texts = []
         all_ids = pub_dic.keys()
-        #all_ids.sort() # sort by abstractID
-        all_ids = sorted(all_ids)
+        all_ids.sort()  # sort by abstractID
         for i in all_ids:
-            if i in pub_dic and i in turk_dic and i in rel_dic:
+            if pub_dic.has_key(i) and turk_dic.has_key(i) and rel_dic.has_key(i):
                 texts.append(pub_dic[i])
             else:
-                if i in pub_dic: pub_dic.pop(i)
-                if i in turk_dic: turk_dic.pop(i)
-                if i in rel_dic: rel_dic.pop(i)
+                if pub_dic.has_key(i): pub_dic.pop(i)
+                if turk_dic.has_key(i): turk_dic.pop(i)
+                if rel_dic.has_key(i): rel_dic.pop(i)
 
-        rel = [rel_dic[i] for i in all_ids if i in rel_dic]
-        #(_, rel) = zip(*rel_dic.items())
-        #rel = map(int, rel)
-        rel = [int(i) for i in rel]
+        (_, rel) = zip(*rel_dic.items())
+        rel = map(int, rel)
 
     vectorizer = TfidfVectorizer(tokenizer=word_tokenize, ngram_range=(1, 2), binary=True, max_features=500)
-    #vectorizer = TfidfVectorizer()
+    # vectorizer = TfidfVectorizer()
     mat = vectorizer.fit_transform(texts)
-    #return (pub_dic, texts)
+    # return (pub_dic, texts)
 
     instances_filtered = pub_dic.keys()
-    instances_filtered = sorted(instances_filtered)
+    instances_filtered.sort()
     orin2idx_instance = {instances_filtered[i]: i for i in range(len(instances_filtered))}
 
     '''
@@ -74,7 +72,7 @@ def loadDec_Feat_Turk(dataset,filepath):
     return mat, rel, orin2idx_instance
 
 
-def get_pub_dic_csv(dataset,filepath):
+def get_pub_dic_csv(dataset, filepath):
     filename = filepath + dataset + "-text.csv"
     f = open(filename)
     f.readline()
@@ -92,7 +90,8 @@ def get_pub_dic_csv(dataset,filepath):
 
     return pub_dic
 
-def loadResponse(dataset,filepath):
+
+def loadResponse(dataset, filepath):
     responses = {}  # response: {instance: {oralce}}
 
     all_oracles = {}
@@ -114,17 +113,21 @@ def loadResponse(dataset,filepath):
         AbstractId = int(AbstractId)
 
         # oracles
-        if WorkerId not in all_oracles: all_oracles[WorkerId] = 1
-        else: all_oracles[WorkerId] += 1
+        if WorkerId not in all_oracles:
+            all_oracles[WorkerId] = 1
+        else:
+            all_oracles[WorkerId] += 1
         # instances
-        if AbstractId not in all_instances: all_instances[AbstractId] = 1
-        else: all_instances[AbstractId] += 1
+        if AbstractId not in all_instances:
+            all_instances[AbstractId] = 1
+        else:
+            all_instances[AbstractId] += 1
 
         if AbstractId not in responses: responses[AbstractId] = {}
         responses[AbstractId][WorkerId] = get_answer(Question3, Question4, dataset)
 
     oracles_filtered = all_oracles.keys()
-    oracles_filtered = sorted(oracles_filtered)
+    oracles_filtered.sort()
     orin2idx_oracle = {oracles_filtered[i]: i for i in range(len(oracles_filtered))}
 
     return responses, orin2idx_oracle
@@ -153,6 +156,7 @@ def get_turk_data(dataset):
 
     return (turk_dic, rel_dic)
 
+
 def get_answer(q3, q4, dataset):
     NEG = 0
     UNOBSERVED = 0
@@ -166,7 +170,7 @@ def get_answer(q3, q4, dataset):
         ans = NEG
         if (q4 == 'CantTell' or q4 == 'Yes'):
             ans = POS
-        #if (q4 == '-' and q3 == '-'):
+        # if (q4 == '-' and q3 == '-'):
         #    ans = UNOBSERVED
 
     return ans
@@ -191,27 +195,33 @@ def get_pub_dic_xml(filepath):
 
     return pub_dic
 
+
 def get_text(a):
     try:
         return a.text
     except AttributeError:
         return ''
 
+
 def get_relevant(filepath):
-    f = open(filepath+'proton-beam-relevant.txt')
+    f = open(filepath + 'proton-beam-relevant.txt')
     res = np.zeros(4751)
     for line in f:
         x = int(line)
-        res[x-1] = 1
+        res[x - 1] = 1
     f.close()
 
+
 def responseOut2In(responses, oracle_idx, instance_idx):
-    inside_response = {instance_idx[abstractID]:{oracle_idx[oid]: responses[abstractID][oid] for oid in responses[abstractID]} for abstractID in responses}
+    inside_response = {
+    instance_idx[abstractID]: {oracle_idx[oid]: responses[abstractID][oid] for oid in responses[abstractID]} for
+    abstractID in responses}
     return inside_response
 
+
 def response2matrix(responses_inside, label_idx, oracle_num, instance_num):
-    matrix= np.zeros([len(label_idx), oracle_num,
-                      instance_num])  # 0 or 1 [lb(0,1,2,..): [oi(0,1,2,..):[instance(0,1,2,..)]]]
+    matrix = np.zeros([len(label_idx), oracle_num,
+                       instance_num])  # 0 or 1 [lb(0,1,2,..): [oi(0,1,2,..):[instance(0,1,2,..)]]]
 
     for id in responses_inside:
         for oi in responses_inside[id]:
@@ -220,12 +230,15 @@ def response2matrix(responses_inside, label_idx, oracle_num, instance_num):
             matrix[li][oi][id] = 1.0
     return matrix
 
+
 '''
 select one oracle
 input:
     oracleid: (inner)
     response: (inner)
 '''
+
+
 def selectInstanceByOracle(responses, v_features, truelabel, oracleid):
     select_instanceid = []
     for id in responses:
@@ -237,11 +250,14 @@ def selectInstanceByOracle(responses, v_features, truelabel, oracleid):
     select_features = v_features[select_instanceid]
     return select_instanceid, select_features, select_label, select_noisylabel
 
+
 '''
 return the oracleid which answers most questions
 input: 
     response: inner
 '''
+
+
 def max_oracle(responses, oracleNum):
     num_answer = [0 for i in range(oracleNum)]
     for id in responses:
@@ -250,6 +266,7 @@ def max_oracle(responses, oracleNum):
     max_oi = num_answer.index(max(num_answer))
     return max_oi
 
+
 def noisylabelQuality(true_labels, noisylabel):
     classes = list(set(true_labels))
     correct = {lb: 0 for lb in classes}
@@ -257,17 +274,17 @@ def noisylabelQuality(true_labels, noisylabel):
     for i in range(len(true_labels)):
         if true_labels[i] == noisylabel[i]:
             correct[true_labels[i]] += 1
-    acc_per_class = {lb: float(correct[lb])/label_num[lb] for lb in correct}
+    acc_per_class = {lb: float(correct[lb]) / label_num[lb] for lb in correct}
     return acc_per_class, label_num
-            
+
 
 '''
     responses: dict{instance: {oracle: annotation}}
 '''
 if __name__ == '__main__':
-    dataName = "appendicitis" #appendicitis
+    dataName = "appendicitis"
     filepath = "../../dataset/dec/"
-    u_features, v_features, responses, matrix_sparse, filter_labels, true_labels = loadDecData(dataName,filepath)
+    u_features, v_features, responses, matrix_sparse, filter_labels, true_labels = loadDecData(dataName, filepath)
     print("oracle num: ", u_features.shape[0], "instance num:", v_features.shape[0])
 
     # find max oi
@@ -275,6 +292,5 @@ if __name__ == '__main__':
     select_instanceid, select_features, select_label, select_noisylabel = \
         selectInstanceByOracle(responses, v_features, true_labels, max_oi)
     acc_per_class, label_num = noisylabelQuality(select_label, select_noisylabel)
-    print("size: ", len(select_instanceid), "oracleid: ", max_oi, "label distri: ", label_num, "oracle quality: ", acc_per_class)
-
-
+    print("size: ", len(select_instanceid), "oracleid: ", max_oi, "label distri: ", label_num, "oracle quality: ",
+          acc_per_class)
